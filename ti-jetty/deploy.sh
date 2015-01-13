101115d4611818628dde71faf3affc0356bc4c80
liufei_it <liufei_it@126.com>
2015-01-13 18:29:01 +0800

    shell

--- ti-jetty/deploy.sh
+++ ti-jetty/deploy.sh
@@ -2,6 +2,6 @@
 
 mvn clean install -Dmaven.test.skip=true
 
-scp -r build/lib andpay@stage0_cfc:~/app/lc/jetty/
-scp -r etc andpay@stage0_cfc:~/app/lc/jetty/
+scp -r build/lib root@aliyun:~/jetty/
+
 
