4281571bba39abab8d3f92d0d9e921ed4a2572bb
liufeiit <liufei_it@126.com>
2015-01-16 01:04:13 +0800

    ii

--- ti-jetty/jetty/bin/start.sh
+++ ti-jetty/jetty/bin/start.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
-java -Dsun.net.inetaddr.ttl=60 -Xmx256M -XX:+HeapDumpOnOutOfMemoryError -DLOG_DIR=/log/java/log -jar ti-jetty-1.0.0-Final.jar
-
-
+java -Dsun.net.inetaddr.ttl=60 -Xmx256M -XX:+HeapDumpOnOutOfMemoryError -jar ti-jetty-1.0.0-Final.jar
